c3111ed9b21b1dd04961702d52e228ff728c0dbe
ertucaymaz-afk <ertucaymaz@gmail.com>
2026-08-19 08:05:26 +0300

    feat(ui): load Design System v2 component surfaces

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./light-theme-v016.css";
 import "./remixrota-v016.css";
 import "./design-system-v2.css";
+import "./design-system-v2-components.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("DevBox renderer root was not found.");
