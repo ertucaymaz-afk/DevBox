1942f659c6c04b3e1b46ffaff928b4b6563c1ac7
ertucaymaz-afk <ertucaymaz@gmail.com>
2026-08-19 19:34:46 +0300

    devapi desktop: expose health diagnostics IPC

--- desktop/devapi/preload.cjs
+++ desktop/devapi/preload.cjs
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('devapiDesktop', Object.freeze({
   listSurfaces: () => ipcRenderer.invoke('devapi:list-surfaces'),
   getSurface: (key) => ipcRenderer.invoke('devapi:get-surface', key),
   getManifest: () => ipcRenderer.invoke('devapi:get-manifest'),
+  getHealth: () => ipcRenderer.invoke('devapi:get-health'),
   getBuildInfo: () => ipcRenderer.invoke('devapi:get-build-info'),
   openExternal: (url) => ipcRenderer.invoke('devapi:open-external', url)
 }));
