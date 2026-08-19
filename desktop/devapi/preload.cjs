496c969717ce585ee6647572ede5ee895d91eda1
ertucaymaz-afk <ertucaymaz@gmail.com>
2026-08-19 21:17:07 +0300

    devapi desktop: expose task workspace IPC

--- desktop/devapi/preload.cjs
+++ desktop/devapi/preload.cjs
@@ -5,6 +5,9 @@ contextBridge.exposeInMainWorld('devapiDesktop', Object.freeze({
   getSurface: (key) => ipcRenderer.invoke('devapi:get-surface', key),
   getManifest: () => ipcRenderer.invoke('devapi:get-manifest'),
   getHealth: () => ipcRenderer.invoke('devapi:get-health'),
+  getLocalState: () => ipcRenderer.invoke('devapi:get-local-state'),
+  createDraftTask: (text) => ipcRenderer.invoke('devapi:create-draft-task', text),
+  pickProject: () => ipcRenderer.invoke('devapi:pick-project'),
   getBuildInfo: () => ipcRenderer.invoke('devapi:get-build-info'),
   openExternal: (url) => ipcRenderer.invoke('devapi:open-external', url)
 }));
